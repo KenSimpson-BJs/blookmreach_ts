e616ae095b496bdeb09c4e1576bc5aa1378ebeed
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2023-05-25 15:48:14 -0500

    updated CTA styles

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { getSelectionValue } from "../utils/general";
+import { getSelectionValue } from "../../utils/general";
+import styles from "./CTA.module.scss";
 
 interface CTA {
   cta?: Cta;
@@ -9,13 +10,15 @@ export function CTA(props: CTA): React.ReactElement | null {
   const { cta, style } = props.cta ?? {};
 
   const returnStyle = (value: string) => {
+    console.log(value);
+
     switch (value) {
       case "primary":
         return `btn btn-primary`;
       case "secondary":
         return `btn btn-secondary`;
       default:
-        return "link";
+        return `${styles.link}`;
     }
   };
 
